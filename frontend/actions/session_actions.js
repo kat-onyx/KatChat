49809a5d964ec93bd23e6d3eebb154702f9b230a
kat <sarah.kathryn.peters@gmail.com>
2018-12-12 12:34:53 -0800

    create action and action creators

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,43 @@
+import * as APIUtil from "../util/session_api_util";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+
+const receiveCurrentUser = (currentUser) => {
+    return {
+        type: RECEIVE_CURRENT_USER,
+        currentUser
+    }
+}
+const logoutCurrentUser = () => {
+    return {
+        type: LOGOUT_CURRENT_USER,
+    }
+}
+
+const receiveErrors = (errors) => {
+    return {
+        RECEIVE_ERRORS,
+        errors
+    }
+}
+
+export const login = (user) => dispatch => {
+    APIUtil.login(user) 
+    .then((resp) => dispatch(receiveCurrentUser(resp))),
+    err => (dispatch(receiveErrors(err.responseJSON)))
+}
+
+export const signup = (user) => dispatch => {
+    APIUtil.signup(user) 
+    .then((resp) => dispatch(receiveCurrentUser(resp))),
+    err => (dispatch(receiveErrors(err.responseJSON)))
+}
+
+export const logout = () => dispatch => {
+    APIUtil.login() 
+    .then((resp) => dispatch(logoutCurrentUser(resp))),
+    err => (dispatch(receiveErrors(err.responseJSON)))
+}

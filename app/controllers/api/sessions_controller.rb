b84670b85b683a2f1869ab7c25b26cf26d48b416
kat <sarah.kathryn.peters@gmail.com>
2018-12-12 09:46:01 -0800

    Finish sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,22 @@
 class Api::SessionsController < ApplicationController
+    def create 
+        @user = User.find_by_credentials(params[:user][:username], [:user][:password])
+
+        if @user
+            login!(@user)
+            render :json "/"
+        else
+            render: ["Invalid login credentials"], status: 401
+        end
+    end
+
+    def destroy
+        @user = current_user
+        if @user 
+            logout 
+            render "api/users/show"
+        else
+            render json: ["No one currently signed in"], status 404
+        end
+    end
 end

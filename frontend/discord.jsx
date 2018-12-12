03a66dca8af923da9f8746e82c9e71498958f7e1
kat <sarah.kathryn.peters@gmail.com>
2018-12-11 17:06:07 -0800

    Moar setup

---
+++ frontend/discord.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    ReactDOM.render(
+        <h1>DISCORD CLONE goes here</h1>
+    )
+    
+})

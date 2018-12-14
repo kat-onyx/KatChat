6f043684e2735801ea81bd652598362f061c29d6
kat <sarah.kathryn.peters@gmail.com>
2018-12-14 12:18:51 -0800

    Hook up server and server_errors reducers to respective combinedReducers

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
+import serverReducer from './server_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
+    servers: serverReducer,
 })
 
 export default entitiesReducer;
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import sessionsErrorReducer from "./session_errors_reducer";
+import serverErrorsReducer from './server_errors_reducer';
 
 const errorsReducer = combineReducers({
     login: sessionsErrorReducer,
+    serverName: serverErrorsReducer,
 })
 
 export default errorsReducer;

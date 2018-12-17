bdbb2184b39919f7499f8b52a44258db068c4a14
kat <sarah.kathryn.peters@gmail.com>
2018-12-17 14:31:43 -0800

    Fix issue for heroku image

--- frontend/components/server/server_index_item.jsx
+++ frontend/components/server/server_index_item.jsx
@@ -4,20 +4,28 @@ import { Link } from 'react-router-dom';
 class ServerIndexItem extends React.Component {
     constructor(props) {
         super(props);
-
+        
         this.state = {
             name: this.props.server.name,
-            id: this.props.server.id
+            id: this.props.server.id,
+            active: false
         }
-
     }
 
+    toggleFocus() {
+        const currentState = this.state.active;
+        this.setState({ active: !currentState})
+    }
 
     render() {
         return (
             <div>
-                <div className="server-icon">
-                    <Link to={`/servers/${this.state.id}`}>{this.state.name[0]}</Link>
+                <div className={this.state.active ? "server-icon.active" : "server-icon"}>
+                    <Link to={`/servers/${this.state.id}`}
+                        onClick={this.toggleFocus}
+                    >
+                        {this.state.name[0]}
+                    </Link>
                 </div>
             </div>
         )

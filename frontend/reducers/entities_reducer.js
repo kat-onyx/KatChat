74354c9e3318b9d9e41823b1712fe6b45bf6d599
kat <sarah.kathryn.peters@gmail.com>
2018-12-19 11:34:03 -0800

    Complete reducer for messages

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import serverReducer from './server_reducer';
 import channelReducer from './channels_reducer';
+import messagesReducer from './messages_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     servers: serverReducer,
     channels: channelReducer,
+    messages: messagesReducer,
 })
 
 export default entitiesReducer;
---
+++ frontend/reducers/messages_reducer.js
@@ -0,0 +1,23 @@
+import merge from 'lodash/merge';
+
+import {
+    RECEIVE_ALL_MESSAGES,
+    RECEIVE_MESSAGE,
+    REMOVE_MESSAGE
+} from '../actions/message_actions';
+
+const messagesReducer = (state = {}, action) => {
+    Object.freeze(state);
+    switch(action.type) {
+        case RECEIVE_ALL_MESSAGES: 
+            return action.messages;
+        case RECEIVE_MESSAGE: 
+            return merge({}, state, { [action.message.id]: action.message})
+            //TODO: may be a problem if state is not updating immediately
+            //TODO: implement remove
+        default:
+            return state;
+    }
+}
+
+export default messagesReducer;

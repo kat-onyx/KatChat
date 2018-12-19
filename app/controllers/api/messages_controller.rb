ea5840a95bb93499dd2b4e8c1392170d36b21ca2
kat <sarah.kathryn.peters@gmail.com>
2018-12-19 10:56:31 -0800

    Add index and show view for messages, add show route in case

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,32 @@
 class Api::MessagesController < ApplicationController
 
     def index
-        @messages = 
+        @messages = Channel.find_by(channel_id: params[:id]);
+    end
+
+    def show
+    end
+
+    def create 
+        @channel = Channel.find(params[:message][:channel_id])
+
+        if @channel 
+            @message = Message.new(message_params)
+            @message.channel_id = params[:message][:channel_id]
+            @message.author_id = current_user.id
+            if @message.save
+                render 'api/messages/show'
+            else
+                render json: @message.errors.full_messages, status: 422
+            end
+        end
+    end
+
+    def update
+
+    end
+
+    def destroy
     end
 
     private 

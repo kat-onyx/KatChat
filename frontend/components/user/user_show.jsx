acf5f577abfac4572e40def85625d9d2e82c0e5f
kat <sarah.kathryn.peters@gmail.com>
2018-12-21 11:16:29 -0800

    Add possible fix for login username issue

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -3,10 +3,14 @@ import React from 'react';
 class UserShow extends React.Component {
 
     render() {
+        let username = null;
+        if (this.props.currentUser) {
+            username = this.props.currentUser.username;
+        }
         return (
             <div className="user-content">
                 <div className="username-box">
-                    {this.props.currentUser.username}
+                    {username}
                 </div>
                 <button onClick={() => this.props.logout()}><span>Logout</span></button>
             </div>

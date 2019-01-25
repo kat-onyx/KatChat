253481e580cbaf4a194366101065be1bd6dd5be6
kat <sarah.kathryn.peters@gmail.com>
2019-01-24 17:21:15 -0800

    Reverting some fixes

--- frontend/components/channels/channel_show.jsx
+++ frontend/components/channels/channel_show.jsx
@@ -10,10 +10,10 @@ class ChannelShow extends React.Component {
         let firstChannel = this.props.channels[channelList[0]];
         let currentServerId = this.props.match.params.serverId;
 
-        if (this.props.match.params.channelId === undefined && channelList.length != 0) {
-            currentChannel = firstChannel.channel_name;
-            this.props.history.push(`/servers/${currentServerId}/channels/${firstChannel.id}`);
-        }
+        // if (this.props.match.params.channelId === undefined && channelList.length != 0) {
+        //     currentChannel = firstChannel.channel_name;
+        //     this.props.history.push(`/servers/${currentServerId}/channels/${firstChannel.id}`);
+        // }
         return (
             <div className="channel-name-box">
                 <div className="channel-name"><span>#</span>{currentChannel}</div>

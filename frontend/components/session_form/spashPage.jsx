c3dc6b0a9b6d09c9b58940d85c6097645c4e0db4
kat <sarah.kathryn.peters@gmail.com>
2019-01-30 13:37:35 -0800

    Finish up CSS, and navigation for splashpage

--- frontend/components/session_form/spashPage.jsx
+++ frontend/components/session_form/spashPage.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 class SplashPage extends React.Component {
 	render() {
 		return (
 			<div className="splash-contents-container">
 				<div className="splash-nav">
-					<div className="splash-nav-title">KatChat</div>
+					<div className="splash-nav-left">
+						<div className="splash-nav-image" />
+						<div className="splash-nav-title">KatChat</div>
+					</div>
+					<div className="splash-nav-right">
+						<a href="https://github.com/kat-onyx">
+							<div className="splash-nav-github-icon" />
+						</a>
+						<a href="https://www.linkedin.com/in/sarah-peters-a2695b8a/">
+							<div className="splash-nav-linkedin-icon" />
+						</a>
+
+						<NavLink to="/login">
+							<button className="splash-nav-login"> Login </button>
+						</NavLink>
+					</div>
 				</div>
 				<div className="splash-about-images">
 					<div className="splash-about">
@@ -14,7 +30,9 @@ class SplashPage extends React.Component {
 							It's time to ditch Skype and Teamspeak. KatChat is a free live
 							chatting app for gamers and non-gamers alike. Simplify your life.
 						</p>
-						<button>Open KatChat</button>
+						<NavLink to="/login">
+							<button className="splash-login-button">Open KatChat</button>
+						</NavLink>
 					</div>
 					<div className="splash-images">
 						<div className="splash-images-inner">
@@ -31,7 +49,15 @@ class SplashPage extends React.Component {
 					</div>
 				</div>
 				<div className="splash-footer">
-					Disclaimer: Assets were borrowed from Discord.
+					<div className="splash-footer-left">
+						<h2> Are you ready to join? </h2>
+						{/* <p>Several assets on this page were borrowed from Discord.</p> */}
+					</div>
+					<div className="splash-footer-right">
+						<NavLink to="/signup">
+							<button>Sign Up Now</button>
+						</NavLink>
+					</div>
 				</div>
 			</div>
 		);
